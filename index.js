60ba88aaebd23d7a6f98c55fca03a6e07cd6ffc6
+ <+>
2023-03-26 16:48:34 +0300

    Done with Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+// Define divide function
+const divide = function () {
+    return 2000/100;
+}
+
+// Define square function
+const square = x => x * x;
+console.log(square(2));
+
+// Define add function
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+console.log(add(3,4));
